6f583c0879f976538034aacc40c728c9bbe457a4
Bolodenis <bollodenis@gmail.com>
2025-01-17 07:27:11 +0000

    menu

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,32 +1,32 @@
 #!/usr/bin/python3
 
-'''Thismodulecontainsthefunctiontop_ten'''
+''' This module contains the function top_ten '''
 import json
 import requests
 
-deftop_ten(subreddit):
-'''Getsthetoptentopicsofasubreddit'''
-url=f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-headers={"User-Agent":"GetHotTopics/1.0"}
+def top_ten(subreddit):
+    ''' Gets the top ten topics of a subreddit '''
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    headers = {"User-Agent": "GetHotTopics/1.0"}
+    
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        
+        # Check if the subreddit exists
+        if response.status_code != 200:
+            print(None)
+            return
 
-try:
-    response=requests.get(url,headers=headers,allow_redirects=False)
-
-#Checkifthesubredditexists
-    if response.status_code!=200:
+        # Parse the JSON response
+        data = response.json()
+        posts = data.get('data', {}).get('children', [])
+        
+        # Print titles of the first 10 hot posts
+        if not posts:
+            print(None)
+        else:
+            for post in posts:
+                print(post['data']['title'])
+    except Exception as e:
         print(None)
-        return
-
-#ParsetheJSONresponse
-    data=response.json()
-    posts=data.get('data',{}).get('children',[])
-
-#Printtitlesofthefirst10hotposts
-    if not posts:
-        print(None)
-    else:
-        for post in posts:
-            print(post['data']['title'])
-except Exception as e:
-print(None)
 
