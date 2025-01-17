ff7beb83d53e8fb51582e7ae3bd0dc5c9dfb1b80
Bolodenis <bollodenis@gmail.com>
2025-01-17 07:05:45 +0000

    menu

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,30 +1,21 @@
 #!/usr/bin/python3
-"""
-number of subscribers for a given subreddit
-"""
+
+''' This module contains the function top_ten '''
+import json
 import requests
 
+
 def top_ten(subreddit):
-    try:
-        # Base URL for Reddit API
-        base_url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-        headers = {'User-Agent': 'Mozilla/5.0'}
+    ''' Gets the top ten topics of subscribers of a subreddit '''
+    response = requests.get("https://www.reddit.com/r/{}/top.json?limit=10"
+                            .format(subreddit),
+                            headers={"User-Agent": "GetHotTopics"},
+                            allow_redirects=False).json()
 
-        # Make the request
-        response = requests.get(base_url, headers=headers, allow_redirects=False)
-
-        # If the subreddit is invalid or returns a redirect
-        if response.status_code == 404:
-            print(None)
-            return
-        
-        # Parsing the JSON response
-        data = response.json()
-        posts = data['data']['children']
-        
-        # Print the titles of the top 10 hot posts
-        for post in posts:
-            print(post['data']['title'])
-    
-    except requests.exceptions.RequestException as e:
-          print(None)
+    data = response.get('data')
+    if not data:
+        print(None)
+    else:
+        titles = []
+        for child in data['children']:
+            print(child['data']['title'])
