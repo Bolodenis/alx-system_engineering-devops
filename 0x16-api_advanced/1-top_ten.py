8ad8f908310c92d64b8d251719d12cac42a22ba1
Bolodenis <bollodenis@gmail.com>
2025-01-17 07:20:14 +0000

    menu

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 '''Thismodulecontainsthefunctiontop_ten'''
-importjson
-importrequests
+import json
+import requests
 
 deftop_ten(subreddit):
 '''Getsthetoptentopicsofasubreddit'''
@@ -10,23 +10,23 @@ url=f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
 headers={"User-Agent":"GetHotTopics/1.0"}
 
 try:
-response=requests.get(url,headers=headers,allow_redirects=False)
+    response=requests.get(url,headers=headers,allow_redirects=False)
 
 #Checkifthesubredditexists
-ifresponse.status_code!=200:
-print(None)
-return
+    if response.status_code!=200:
+        print(None)
+        return
 
 #ParsetheJSONresponse
-data=response.json()
-posts=data.get('data',{}).get('children',[])
+    data=response.json()
+    posts=data.get('data',{}).get('children',[])
 
 #Printtitlesofthefirst10hotposts
-ifnotposts:
-print(None)
-else:
-forpostinposts:
-print(post['data']['title'])
-exceptExceptionase:
+    if not posts:
+        print(None)
+    else:
+        for post in posts:
+            print(post['data']['title'])
+except Exception as e:
 print(None)
 
