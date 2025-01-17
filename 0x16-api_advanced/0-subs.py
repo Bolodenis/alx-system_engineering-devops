a05dca1cb20c40148251d2777144f547e2d80e6e
Bolodenis <bollodenis@gmail.com>
2025-01-17 07:47:44 +0000

    m

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,7 +4,7 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """Print the total number of subscribers on a given subreddit."""
+    """Return the total number of subscribers on a given subreddit."""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
@@ -12,10 +12,9 @@ def number_of_subscribers(subreddit):
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
         if response.status_code != 200:
-            print(0)
-            return
+            return 0
         results = response.json().get("data", {})
-        print(results.get("subscribers", 0))
+        return results.get("subscribers", 0)
     except Exception:
-        print(0)
+        return 0
 
